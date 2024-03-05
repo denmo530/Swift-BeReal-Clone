5af7e7a932364973b56e789d0270675f1ed36d92
Dennis <dennis.moradkhani@gmail.com>
2024-03-04 17:27:04 -0700

    Initial Commit

---
+++ bereal-clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bereal-clone
+//
+//  Created by Dennis Moradkhani on 2024-03-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bereal-clone/bereal_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  bereal_cloneApp.swift
+//  bereal-clone
+//
+//  Created by Dennis Moradkhani on 2024-03-04.
+//
+
+import SwiftUI
+
+@main
+struct bereal_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
